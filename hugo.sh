f31d58631070be3decd50375ea5e9be780692655
acheng <2904817126@qq.com>
2023-06-10 16:54:27 +0800

    first commit

--- hugo.sh
+++ hugo.sh
@@ -1,18 +1,6 @@
-echo "git add *" >> hugo.sh
-echo "git commit -m \"first commit\"" >> hugo.sh
-echo "git push" >> hugo.sh
-chmod a+x hugo.shgit add *
-git commit -m "first commit"
-git push
-git add *
-git commit -m "first commit"
-git push
-git add *
-git commit -m "first commit"
-git push
-git add *
-git commit -m "first commit"
-git push
+#\!/bin/sh
+cd /Users\cheng\quickstart\public
+cd /Users\cheng\bocuo5.github.io
 git add *
 git commit -m "first commit"
 git push
